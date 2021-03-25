946022ad82c8f38f8de99ad4cc2f5be6c042bce1
Karen Sniquer <kbsniquer@gmail.com>
2021-03-25 12:44:49 -0400

    added inputs & dropdowns to form

--- src/components/RecipeForm.vue
+++ src/components/RecipeForm.vue
@@ -1,37 +1,82 @@
 <template>
   <div class="container">
-    <form @submit.prevent="addRecipe()" class="shadow-sm p-5">
+    <form @submit.prevent="addRecipe()" class="p-5">
       <h1>Add New Recipe</h1>
-      <div class="form-group row p-2">
-        <label for="recipeTitle" class="col-sm-3 col-form-label"
-          >Recipe Title</label
-        >
-        <div class="col-sm-9">
-          <input
-            type="text"
-            class="form-control"
-            v-model="newRecipeTitle"
-            placeholder="Enter a title for your recipe"
-          />
+      <!-- Recipe title form group with label & input -->
+      <div class="form-row p-2">
+        <div class="form-group col-md-12">
+          <label for="recipeTitle" class="control-label">Recipe Title</label>
+          <div class="col-sm-12">
+            <input
+              type="text"
+              class="form-control"
+              v-model="newRecipeTitle"
+              placeholder="Enter a title for your recipe"
+            />
+          </div>
         </div>
       </div>
-      <div class="form-group row p-2">
-        <label for="recipeDesc" class="col-sm-3 col-form-label"
-          >Recipe Description</label
-        >
-        <div class="col-sm-9">
-          <textarea
-            class="form-control"
-            name="recipeDesc"
-            v-model="newRecipeDesc"
-            cols="30"
-            rows="5"
-            placeholder="Enter a description for your recipe..."
-          ></textarea>
+      <!-- Row with dropdowns for types of recipes & cook times -->
+      <div class="form-row-dropdowns">
+        <select name="recipeCookTimeDropdown">
+          <option
+            class="dropdown-item"
+            v-for="(time, index) in recipeCookTimes[0]"
+            :key="index"
+            >{{ time }}</option
+          >
+        </select>
+
+        <select name="recipeCategoryDropdown">
+          <option
+            class="dropdown-item"
+            v-for="(category, index) in recipeCategories[0]"
+            :key="index"
+            >{{ category }}</option
+          >
+        </select>
+      </div>
+      <!-- Recipe ingredients text area, ingredients separated by comma for gallery ul -->
+      <div class="form-row p-2">
+        <div class="form-group col-md-12">
+          <label for="recipeIngredients" class="control-label"
+            >Recipe Ingredients</label
+          >
+          <div class="col-sm-12">
+            <textarea
+              class="form-control"
+              name="recipeDesc"
+              v-model="newRecipeIngredients"
+              cols="30"
+              rows="2"
+              placeholder="Enter recipe ingredients separated by commas..."
+            ></textarea>
+          </div>
         </div>
       </div>
+      <!-- Recipe description text area -->
+      <div class="form-row p-2">
+        <div class="form-group col-md-12">
+          <label for="recipeTitle" class="control-label"
+            >Recipe Description</label
+          >
+          <div class="col-sm-12">
+            <textarea
+              class="form-control"
+              name="recipeDesc"
+              v-model="newRecipeDesc"
+              cols="30"
+              rows="4"
+              placeholder="Enter a description for your recipe..."
+            ></textarea>
+          </div>
+        </div>
+      </div>
+
       <div>
-        <button class="btn btn-primary m-3" type="submit">Add Recipe</button>
+        <button class="btn btn-primary m-3" type="submit">
+          Add New Recipe
+        </button>
       </div>
     </form>
   </div>
@@ -43,6 +88,26 @@ export default {
   data: function() {
     return {
       recipes: [],
+      recipeCategories: [
+        {
+          select: "Select Category",
+          breakfast: "Breakfast",
+          lunch: "Lunch",
+          dinner: "Dinner",
+          snack: "Snack",
+        },
+      ],
+      recipeCookTimes: [
+        {
+          select: "Select Cook Time",
+          zeroToFifteen: "0 - 15 mins",
+          fifteenToThirty: "15 - 30 mins",
+          thirtyToFourtyFive: "30 - 45 mins",
+          fourtyFiveToHour: "45 mins - 1hr",
+          hourPlus: "1hr or more",
+        },
+      ],
+      recipeIngredients: [],
       newRecipeTitle: "",
       newRecipeDesc: "",
     };
@@ -74,5 +139,21 @@ export default {
 <style scoped>
 form {
   padding-top: 50px;
+  width: 75%;
+  margin: 0 auto;
+}
+select {
+  border: 2px solid #00bef0;
+  color: #00bef0;
+  margin: 0.5rem;
+  padding: 0.75rem;
+}
+.control-label {
+  float: left;
+}
+.form-row-dropdowns {
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  padding: 1rem;
 }
 </style>
