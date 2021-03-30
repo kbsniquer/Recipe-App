21b2c9412fa2d8650df3c3e479d83f237c27c87a
Karen Sniquer <kbsniquer@gmail.com>
2021-03-30 10:12:33 -0400

    more changes

--- src/App.vue
+++ src/App.vue
@@ -26,6 +26,25 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+label {
+  color: #00bef0;
+  font-weight: bolder;
+}
+.btn {
+  background-color: transparent;
+  border: 2px solid #00bef0;
+  color: #00bef0;
+  padding: 10px 20px;
+  transition: background-color 0.2s, border-color 0.2s, color 0.2s;
+  border-radius: 25px;
+}
+.btn:hover {
+  background-color: #01a5ce;
+  border: 2px solid #01a5ce;
+  border-radius: 25px;
+}
+.container {
+  padding: 3.5rem;
 }
 </style>
