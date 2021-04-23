6edc6cbf8a05273885a2d21e0e8b42c4d6d957e1
kbsniquer <65562500+kbsniquer@users.noreply.github.com>
2021-04-23 14:12:11 -0400

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,5 @@
-build: {
-index: path.resolve(__dirname, '../docs/index.html'),
-assetsRoot: path.resolve(__dirname, '../docs'),
-assetsSubDirectory: 'static',
-assetsPublicPath: '',
-productionSourceMap: true,
-devtool: '#source-map',
-productionGzip: false,
-productionGzipExtensions: ['js', 'css'],
-bundleAnalyzerReport: process.env.npm_config_report
+module.exports = {
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/Recipe-App/'
+    : '/'
 }
